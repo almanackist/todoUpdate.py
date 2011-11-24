5227059fd12138249d607c187e8caee6b9f3aae3
almanackist <c.alan.joyce@gmail.com>
2011-11-23 21:56:58 -0600

    Fixed directory

--- todoMaintenance.sh
+++ todoMaintenance.sh
@@ -3,4 +3,4 @@ td=$(date '+%F')
 /Users/alanj/Downloads/todo.txt_cli-2.7/todo.sh add "(A) Review Jira queue *$td"
 /Users/alanj/Downloads/todo.txt_cli-2.7/todo.sh archive
 cp /Users/alanj/Dropbox/todo/todo.txt /Users/alanj/Dropbox/todo/todo.txt.archive
-python "/Users/alanj/Documents/Python experiments/todoUpdate.py"
+python "/Users/alanj/Documents/Python experiments/todoUpdate/todoUpdate.py"
